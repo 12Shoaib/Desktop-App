5960bae7ff27d8733a3fc01df20050f8e192c060
Mohd <Shoaibmohd916@gmail.com>
2023-06-26 03:50:38 -0700

    app initialisation

---
+++ Script.js
@@ -0,0 +1,8 @@
+// for (var i = 0; i <= 10; i++) {
+//   console.log(i)
+//   // setTimeout(() => {
+//   //   console.log(i);
+//   // }, i * 1000);
+// }
+
+console.log('The python script started running')
---
+++ src/App.js
@@ -0,0 +1,43 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  function callPyhtonFile() {
+    const { exec } = window.require('child_process')
+
+      exec('node Script.js', (error, stdout, stderr) => {
+      if (error) {
+        console.error(`Error: ${error.message}`);
+        return;
+      }
+      if (stderr) {
+        console.error(`stderr: ${stderr}`);
+        return;
+      }
+      console.log(`stdout: ${stdout}`);
+    })
+  }
+
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <button onClick={callPyhtonFile}>callPythonFile</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
